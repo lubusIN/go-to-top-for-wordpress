1b8f8344e6442b6e95479dd299012ccdcdf97c55
Ajit Bohra <ajit@lubus.in>
2023-10-22 18:21:42 +0530

    fix settings sidebar

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0248b8577b0b3a2f1159');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '67ce8e610b87a437c3d6');
